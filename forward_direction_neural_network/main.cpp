8645ef2b59e0bbf738892e48031de412d3febb26
savoptik <sav70792@gmail.com>
2018-10-08 21:56:03 +0300

    Описан класс парсер параметров командной строки
    Назначение параметров командной строки можно узнать в файле help.txt, или запустив приложение с параметром -h

--- forward_direction_neural_network/main.cpp
+++ forward_direction_neural_network/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include "parametrsStorage/parametrsStorage.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    parametrsStorage ps(argc, argv);
+    if (ps.operatingMode() == 0) {
+        return 0;
+    }
     return 0;
 }
